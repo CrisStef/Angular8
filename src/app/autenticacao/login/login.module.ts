e27db90512845b82b02ad3bcb010a46f383bc679
Cris Stef <cristiane.souza1234@hotmail.com>
2019-08-05 23:08:30 -0300

    Login com acesso a API

--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -2,18 +2,43 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatListModule} from '@angular/material/list';
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatSnackBarModule
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { LoginComponent, LogarComponent } from './components';
-
-
+import { LoginService } from './services';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule,
+    MatListModule
   ],
   declarations: [
     LoginComponent,
     LogarComponent
+  ],
+  providers: [
+    LoginService
   ]
 })
 export class LoginModule { }
